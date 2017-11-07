2253515209f91e673ca59c544fb098074cf1f89d
Matthieu Laneuville <mlaneuville@gmail.com>
2017-11-07 16:19:36 +0900

    evolution: misc pylint cleanup

--- thermal-evolution.py
+++ thermal-evolution.py
@@ -1,43 +1,17 @@
-# coding: utf-8
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from scipy.integrate import odeint
-from scipy.interpolate import interp1d
-from scipy.optimize import fsolve
-from math import exp
-from math import sqrt
-from glob import glob
-from numpy import random
 import argparse
 import json
 import sys
 import pickle
 
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import random
+from scipy.integrate import odeint
+from scipy.interpolate import interp1d
+from scipy.optimize import fsolve
+
 plt.rcParams.update({'font.size': 16, 'axes.labelsize': 'large'})
 frac = 0.94 # updated with command line
-
-with open("fit.p", "rb") as f:
-    fit = pickle.load(f)
-
-def Mg_to_PCS(Mg):
-    pcs = np.linspace(83, 97, 500)
-    inv = []
-    for value in Mg:
-        v = np.polynomial.polynomial.polyval(frac, fit) - value
-        inv.append(pcs[np.where(v > 0)[0][-1]])
-    return np.array(inv)
-
-def PCS_to_Mg(target):
-    target_ = target*100
-    pcs = np.linspace(83, 97, 500)
-    ret = []
-    for p in target_:
-        idx = np.where(p > pcs)[0][-1]
-        mg = np.polynomial.polynomial.polyval(frac, fit)[idx]
-        ret.append(max(mg, 0))
-    return ret
 
 G = 6.67e-11 # gravitational constant, m3/kg/s2
 YEAR = 365*24*3600. # a year in seconds
@@ -59,6 +33,29 @@ GRAD_TL = 2.5e-4 #
 # orbital
 A0 = RE
 
+with open("fit.p", "rb") as f:
+    fit = pickle.load(f)
+
+def Mg_to_PCS(Mg):
+    '''Converts a Mg# to an equivalent PCS value given plag fraction.'''
+    pcs = np.linspace(83, 97, 500)
+    inv = []
+    for value in Mg:
+        v = np.polynomial.polynomial.polyval(frac, fit) - value
+        inv.append(pcs[np.where(v > 0)[0][-1]])
+    return np.array(inv)
+
+def PCS_to_Mg(target):
+    '''Converts a PCS value to an equivalent Mg# given plag fraction.'''
+    target_ = target*100
+    pcs = np.linspace(83, 97, 500)
+    ret = []
+    for p in target_:
+        idx = np.where(p > pcs)[0][-1]
+        mg = np.polynomial.polynomial.polyval(frac, fit)[idx]
+        ret.append(max(mg, 0))
+    return ret
+
 def area(r):
     '''Returns area of the shell at radius RM-r.'''
     a = 4.*np.pi*(RM-r)**2
@@ -70,12 +67,12 @@ def volume(r):
     return v
 
 def partitioning(y):
-    '''Takes crustal thickness as input and returns PCS.'''    
+    '''Takes crustal thickness as input and returns PCS.'''
     mean_crust = 0.5*(y[0]+y[2])
     v = 4.*np.pi*(RM**3 - (RM-mean_crust)**3)/3
     return 0.85 + v/VOL_MANTLE
 
-def isotherm(y, t, phi, T=800):
+def isotherm(y, T=800):
     '''Returns the depth of an isotherm for a given timestep.'''
     TS = 250
     TM = 1600
@@ -97,6 +94,7 @@ def radio(t):
     return heat
 
 def get_depths(y, mixing='normal-middle', X=20e3):
+    '''Generate a distribution of depth-of-origin given a mixing model.'''
     N = 1000
     depths_ns, depths_fs = [], []
     mu, std = {}, {}
@@ -130,11 +128,11 @@ def get_depths(y, mixing='normal-middle', X=20e3):
             std['fs'] = db_std[mixmodel[1]]['fs']
         while len(depths_ns) < N:
             r = random.normal(loc=mu['ns'], scale=std['ns'])
-            if r > 1 and r < max(y[:,0]):
+            if r > 1 and r < max(y[:, 0]):
                 depths_ns.append(r)
         while len(depths_fs) < N:
             r = random.normal(loc=mu['fs'], scale=std['fs'])
-            if r > 1 and r < max(y[:,2]):
+            if r > 1 and r < max(y[:, 2]):
                 depths_fs.append(r)
     else:
         print("This shouldn't happen.")
@@ -143,9 +141,10 @@ def get_depths(y, mixing='normal-middle', X=20e3):
     return depths_ns, depths_fs
 
 def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=''):
-    
+    '''Generate a series of plots stored in img/'''
+
     # crust size evolution
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
 
     ax.plot(time, y[:, 0]/1e3, 'r', label='nearside', lw=2)
     if run != "symmetrical":
@@ -156,11 +155,11 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     ax.set_xlim(0, max(time))
     ax.set_ylim(0, 70)
     ax.grid()
-    
+
     plt.savefig("img/evolution"+suffix+".png", format="png", bbox_inches="tight")
 
     # isotherm depth
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
 
     ax.plot(time, iso_n/1e3, 'r', label='nearside', lw=2)
     if run != "symmetrical":
@@ -171,12 +170,12 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     ax.set_xlim(0, max(time))
     ax.set_ylim(0, 1.05*max(iso_n)/1e3)
     ax.grid()
-    
+
     plt.savefig("img/isotherms"+suffix+".png", format="png", bbox_inches="tight")
 
     # composition profile [in Mg#]
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
-    
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
+
     ax.plot(PCS_to_Mg(c), y[:, 0]/1e3, 'r', label='nearside', lw=2)
     if run != "symmetrical":
         ax.plot(PCS_to_Mg(c), y[:, 2]/1e3, 'g', label='farside', lw=2)
@@ -185,11 +184,12 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     ax.set_ylabel("Crustal thickness [km]")
     ax.set_ylim(70, 0)
     ax.grid()
-    
+
     plt.savefig("img/composition"+suffix+".png", format="png", bbox_inches="tight")
 
     # orbital distance
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
+
     ax.plot(time, a, 'k', lw=2)
     ax.set_xlabel("Time [Ma]")
     ax.set_ylabel("Semimajor axis [R$_{earth}$]")
@@ -201,7 +201,8 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     plt.savefig("img/semimajor"+suffix+".png", format="png", bbox_inches="tight")
 
     # surface properties
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
+
     ax.plot(time, hfs_n, 'r', lw=2, label='nearside')
     ax.plot(time, hfs_f, 'g', lw=2, label='farside')
     ax.axhline(1361, color='k', ls='--')
@@ -216,8 +217,8 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     plt.savefig("img/surface_hf"+suffix+".png", format="png", bbox_inches="tight")
 
     # composition sample
-    f, ax = plt.subplots(1, 1, figsize=(8, 8))
-    
+    _, ax = plt.subplots(1, 1, figsize=(8, 8))
+
     depths_ns, depths_fs = get_depths(y, mixing=mixing)
     compo_NS = interp1d(y[:, 0], PCS_to_Mg(c))
     compo_FS = interp1d(y[:, 2], PCS_to_Mg(c))
@@ -225,16 +226,30 @@ def plot_results(time, y, iso_n, iso_f, c, a, hfs_n, hfs_f, mixing, run, suffix=
     bins = np.linspace(40, 80, 41)
     ax.hist(compo_NS(depths_ns), bins=bins, normed=1, facecolor='red', alpha=0.5, label='nearside')
     if run != "symmetrical":
-        ax.hist(compo_FS(depths_fs), bins=bins, normed=1, facecolor='green', alpha=0.5, label='farside')
+        ax.hist(compo_FS(depths_fs), bins=bins, normed=1,
+                facecolor='green', alpha=0.5, label='farside')
         ax.legend(loc='best')
 
     ax.set_xlabel("Magnesium number [-]")
     ax.set_ylabel("Normalized distribution")
-    ax.set_xlim(50,75)
+    ax.set_xlim(50, 75)
     ax.set_yticks(np.arange(0, 0.55, 0.1))
     ax.grid()
-    
+
     plt.savefig("img/distribution"+suffix+".png", format="png", bbox_inches="tight")
+
+def generate_runs(ARGS):
+    '''Generate a list of parameter sets to explore phase space.'''
+    parameters = []
+    for i in range(ARGS.num):
+        if ARGS.delay is None:
+            delay = random.random()
+        else:
+            delay = ARGS.delay
+        parameters.append({'run': ARGS.run, 'mixing': ARGS.mixing,
+                           'delay': delay, 'k2Q': ARGS.k2Q, 'plag': ARGS.plag})
+
+    return parameters
 
 
 class Evolution:
@@ -243,7 +258,7 @@ class Evolution:
         self.mixing = mixing
         self.time = np.logspace(-6, 2, 1000) # in Ma
         self.output = 0
-        self.compo= 0
+        self.compo = 0
         self.k2Q = k2Q
         self.plag = plag
         self.semimajor = A0
@@ -259,16 +274,17 @@ class Evolution:
         return max(A0, r)
 
     def get_heat_flow(self, side, t, d, phi, model):
-        '''Heat flow from a conductive profile in the crust. Maximum value is set to the radiative heat flow.'''
+        '''Heat flow from a conductive profile in the crust. Maximum value is
+           set to the radiative heat flow.'''
         self.semimajor = self.orbital_distance(t)/RE
-        
+
         S0 = 1361
         Ts = 331
         sig = 5.67e-8
         if side == 'NS' and model != 'symmetrical':
             Ts = ((S0/2 + 4*S0*(5/self.semimajor)**2)/sig)**0.25
 
-            
+
         q = 2*(1600-Ts)/min(d, np.sqrt(t*MA*1e-6))
         return q
 
@@ -280,20 +296,21 @@ class Evolution:
         '''
         denumFS = area(y[2])*LATENT*RHO + RHO*volume(y[2])*CP*GRAD_TL
         numFS = (self.get_heat_flow('FS', t, y[2], phi, model)*area(0) - radio(t)*RHO*VOL_MANTLE)
-    
+
         denumNS = area(y[0])*LATENT*RHO + RHO*volume(y[0])*CP*GRAD_TL
         numNS = (self.get_heat_flow('NS', t, y[0], phi, model)*area(0) - radio(t)*RHO*VOL_MANTLE)
-        
+
         if model == "delay" and t < delay:
             denumNS *= 1e5
-        
+
         return np.array([y[1], MA*numNS/denumNS, y[3], MA*numFS/denumFS])
 
     def solve(self, phi=0.0):
         yinit = np.array([1.0, 0.0, 1.0, 0.0])
-        self.output = odeint(self.deriv, yinit, self.time, args=(self.run, phi, self.delay), rtol=1e-1)
+        self.output = odeint(self.deriv, yinit, self.time,
+                             args=(self.run, phi, self.delay), rtol=1e-1)
 
-        tfinal = np.where(self.output[:,2] > 70e3)[0][0]
+        tfinal = np.where(self.output[:, 2] > 70e3)[0][0]
         self.output = self.output[:tfinal]
         self.time = self.time[:tfinal]
 
@@ -306,15 +323,15 @@ class Evolution:
 
         for i, t in enumerate(self.time):
             self.compo[i] = partitioning(self.output[i])
-            self.iso_n[i], self.iso_f[i] = isotherm(self.output[i], t, phi)
+            self.iso_n[i], self.iso_f[i] = isotherm(self.output[i])
             self.hfs_n[i] = self.get_heat_flow('NS', t, self.output[i][0], phi, self.run)
             self.hfs_f[i] = self.get_heat_flow('FS', t, self.output[i][2], phi, self.run)
             self.orbit[i] = self.orbital_distance(t)/RE
 
         print("tfinal: %.1f Ma, crust ns/fs: %.1f/%.1f km" % (
-              self.time[-1],
-              self.output[-1, 0]/1e3,
-              self.output[-1, 2]/1e3))
+            self.time[-1],
+            self.output[-1, 0]/1e3,
+            self.output[-1, 2]/1e3))
 
     def get_mg_distribution(self, mixing='normal-middle'):
         y = self.output
@@ -336,37 +353,24 @@ class Evolution:
 
     def plot(self, args):
         suffix = '-'
-        for k,v in args.items():
+        for k, v in args.items():
             if k == 'delay':
-                suffix += "%s:%.2f-" % (k,v)
+                suffix += "%s:%.2f-" % (k, v)
             else:
-                suffix += "%s:%s-" % (k,v)
+                suffix += "%s:%s-" % (k, v)
         suffix = suffix[:-1]
 
-        plot_results(self.time, 
-                     self.output, 
+        plot_results(self.time,
+                     self.output,
                      self.iso_n,
                      self.iso_f,
-                     self.compo, 
+                     self.compo,
                      self.orbit,
                      self.hfs_n,
                      self.hfs_f,
                      mixing=self.mixing,
                      run=self.run,
                      suffix=suffix)
-
-def generate_runs(ARGS):
-    '''Generate a list of parameter sets to explore phase space.'''
-    parameters = []
-    for i in range(ARGS.num):
-        if ARGS.delay is None:
-            delay = random.random()
-        else:
-            delay = ARGS.delay
-        parameters.append({'run': ARGS.run, 'mixing': ARGS.mixing,
-                           'delay': delay, 'k2Q': ARGS.k2Q, 'plag': ARGS.plag})
-
-    return parameters
 
 
 if __name__ == '__main__':
@@ -384,7 +388,7 @@ if __name__ == '__main__':
     PARSER.add_argument('--plag', type=float, default=0.94,
                         help="Plag fraction in anorthosite")
     PARSER.add_argument('--k2Q', type=float, default=0.024,
-                        help="Delay in Ma between near and farside cooling")
+                        help="k2/Q ratio for the Earth")
     PARSER.add_argument('--plot', default=False, action="store_true",
                         help="If true, plot run output in img/ folder")
     PARSER.add_argument('-n', '--num', default=10, type=int,
@@ -394,7 +398,7 @@ if __name__ == '__main__':
     frac = ARGS.plag
     d = generate_runs(ARGS)
     out_stats = []
-    
+
     for i, params in enumerate(d):
         print(params)
         s = Evolution(**params)
@@ -404,7 +408,7 @@ if __name__ == '__main__':
             s.plot(params)
 
         stats = s.get_mg_distribution()
-    
+
         params['nearside_mean'] = stats[0]
         params['nearside_std'] = stats[1]
         params['farside_mean'] = stats[2]
@@ -416,6 +420,5 @@ if __name__ == '__main__':
         params['farside_crust'] = crustfs
         params['front'] = front
 
-    
     with open('dat/data.txt', 'w') as outfile:
         json.dump(d, outfile)
