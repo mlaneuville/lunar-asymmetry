5afcbf22fd1551dfc573799c67f81ce4270b6d82
Matthieu Laneuville <mlaneuville@gmail.com>
2017-11-09 15:18:47 +0900

    impact: more flexible impact category definitions

--- impact-modeling.py
+++ impact-modeling.py
@@ -55,15 +55,8 @@ def max_excavation(L, loc="farside"):
 
     return Dtr*0.12
 
-def get_impact_distribution(era=''):
+def get_impact_distribution(dmin, dmax):
     '''Define impact population density.'''
-    dmin, dmax = 1, 50
-    if era == 'large':
-        dmin, dmax = 20, 40
-    if era == 'small':
-        dmin, dmax = 1, 20
-    if era == 'giant':
-        dmin, dmax = 40, 50
 
     def get_constant(dmin, dmax):
         '''Computes normalization factor for density distribution'''
@@ -91,11 +84,7 @@ class Surface:
         self.output = []
 
         self.gridsize = n
-        self.d = {}
-        self.d['large'] = get_impact_distribution(era='large')
-        self.d['small'] = get_impact_distribution(era='small')
-        self.d['giant'] = get_impact_distribution(era='giant')
-        self.d['all'] = get_impact_distribution(era='')
+        self.d = get_impact_distribution(0, 150)
         
         for i in range(self.gridsize):
             for j in range(self.gridsize):
@@ -108,10 +97,10 @@ class Surface:
     def save_state(self):
         self.output.append(deepcopy(self.field[:,:,0]))
     
-    def generate_impact(self, era='all'):
+    def generate_impact(self):
         '''xy position and impact diameter'''
         x, y = random.random(), random.random()
-        L = self.d[era].rvs()
+        L = self.d.rvs()
         return (x,y), L
     
     def impact(self, x, y, L, loc):
@@ -154,7 +143,7 @@ class Surface:
         self.field = field2
         return
 
-    def plot(self, suffix, show):
+    def plot(self, bins, counts, suffix, show):
         fig, axarr = plt.subplots(1, 1, figsize=(8, 8))
 
         im = axarr.imshow(self.output[-1][:,:].T, extent=[0, 50, 50, 0])
@@ -170,11 +159,9 @@ class Surface:
         if show:
             plt.show()
 
-        bins = np.linspace(0, 100, 40)
-        data, labels = [], []
-        for i in np.linspace(1, len(self.output)-1, 3):
+        data = []
+        for i in range(len(self.output)):
             data.append(np.reshape(self.output[int(i)], (100**2,)))
-            labels.append(int(i))
             
         mu = np.mean(data[-1])
         std = np.std(data[-1])
@@ -182,12 +169,10 @@ class Surface:
         
         fig, axarr = plt.subplots(1, 1, figsize=(8, 8))
 
-        labels = ['After D > 40 km impacts',
-                  'After 20 < D < 40 km impacts',
-                  'After D < 20 km impacts']
-
-        for epoch, l in zip(data, labels):
-            x, bins, _ = plt.hist(epoch, bins=bins, normed=True, alpha=0.8, label=l)
+        xbins = np.linspace(0, 100, 40)
+        for cat, count, val in zip(bins[::-1], counts[::-1], data):
+            l = "After %d %d < L < %d km impacts" % (count, cat[0], cat[1]) 
+            x, bins, _ = plt.hist(val, bins=xbins, normed=True, alpha=0.8, label=l)
 
         axarr.set_xlim(0, 50)
         axarr.legend(loc="best")
@@ -199,50 +184,44 @@ class Surface:
         if show:
             plt.show()
 
-def main(rheo, giant, large, small, show=False):
+def main(rheo, counts, bins, show=False):
     moon = Surface(100)
 
-    history = [{'era':'giant', 'number':giant, 'output':min(giant,2)},
-               {'era':'large', 'number':large, 'output':min(large,100)},
-               {'era':'small', 'number':small, 'output':min(small,5000)}]
+    for count, cat in zip(counts[::-1], bins[::-1]):
+        print("%d impacts, with %d < L < %d km" % (count, cat[0], cat[1]))
+        n_impacts = count
 
-    for epoch in history:
-        print(epoch)
-        n_impacts = epoch['number']
-        era = epoch['era']
-        output = epoch['output']
-
+        moon.d = get_impact_distribution(cat[0], cat[1])
         for i in range(n_impacts):
-            (x, y), L = moon.generate_impact(era=era)
+            (x, y), L = moon.generate_impact()
             moon.impact(x, y, L, loc=rheo)
         
-            if ((i % output) == output-1) or (i == n_impacts - 1):
-                print(i)
-                moon.save_state()
+        moon.save_state()
 
     fname = 'dat/moon-surface'
-    suffix = ''
-    for epoch in history:
-        if epoch['number'] > 0:
-            suffix += '-%s%d' % (epoch['era'], epoch['number'])
+    suffix = '-giant%d' % counts[-1]
     suffix += '-%s' % rheo
     fname += suffix+'.p'
 
     with open(fname, "wb") as f:
         pickle.dump(moon.output, f)
 
-    moon.plot(suffix, show)
+    moon.plot(bins, counts, suffix, show)
 
-def get_impactors_count(giant):
+def get_impactors_count(giant, bins):
     '''Return number of impacts per bin for a given number of giant imapcts.'''
-    a = giant/40**(-3) # N(x > D) = a D**(-3)
-    large = int(a*20**(-3) - giant)
-    small = int(a - large - giant)
+    a = giant/bins[-1][0]**(-2.2) # N(x > D) = a D**(-3)
+    count = [a]
     print("Total number of impacts: %d" % a)
-    print("\t- %d with D > 40 km" % giant)
-    print("\t- %d with 20 < D < 40 km" % large)
-    print("\t- %d with D < 20 km" % small)
-    return (giant, large, small)
+    for b in bins[1:]:
+        c = a*b[0]**(-2.2)
+        count.append(c)
+        print("\t- %.1f > %d km" % (c, b[0]))
+    for i, c in enumerate(count):
+        if i == len(count)-1:
+            continue
+        count[i] -= count[i+1]
+    return [int(x) for x in count]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -257,13 +236,15 @@ if __name__ == "__main__":
                         help="Toggle to show the figures at the end")
     args = parser.parse_args()
 
+    bins = [(1, 20), (20, 100), (100, 150)]
+    count = get_impactors_count(args.basins, bins)
+
     if not args.load:
-        giant, large, small = get_impactors_count(args.basins)
-        main(args.rheo, giant, large, small, args.show)
+        main(args.rheo, count, bins, args.show)
     else:
         with open(args.load, "rb") as f:
             surface = pickle.load(f)
         suffix = "-"+"-".join(args.load.split("-")[2:6])[:-2]
         moon = Surface(100)
         moon.output = surface
-        moon.plot(suffix, args.show)
+        moon.plot(bins, count, suffix, args.show)
